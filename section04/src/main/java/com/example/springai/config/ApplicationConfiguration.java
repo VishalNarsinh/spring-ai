aed2296920a15bbad636b950eeac44b07df82aac
VishalNarsinh <vishalnarsinh@gmail.com>
2026-02-27 11:08:24 +0530

    feat: configure JDBC chat memory repository and update application properties

--- section04/src/main/java/com/example/springai/config/ApplicationConfiguration.java
+++ section04/src/main/java/com/example/springai/config/ApplicationConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +21,11 @@ public class ApplicationConfiguration {
 		return chatClientBuilder.defaultAdvisors(List.of(new SimpleLoggerAdvisor(), new TokenUsageLoggerAdvisor()))
 				.defaultUser("How can you help me?")
 				.build();
+	}
+
+	@Bean
+	public ChatMemory chatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository) {
+		return MessageWindowChatMemory.builder().maxMessages(10).chatMemoryRepository(jdbcChatMemoryRepository).build();
 	}
 
 	@Bean("chatMemoryChatClient")
